664440c23668db0214fb298a4832b6718c92478f
Ihuri Carvalho Barboza <ihuri@outlook.com.br>
2022-01-06 19:48:58 -0300

    crud pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -9,6 +9,47 @@
              return res.status(500).json(error.message)
          }
      }
+     static async pegarUmaPessoa(req, res) {
+         const { id } = req.params
+         try {
+             const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+             return res.status(200).json(umaPessoa)
+         } catch (error) {
+             return res.status(500).json(error.message)
+         }
+     }
+
+     static async criaPessoa(req, res) {
+         const novaPessoa = req.body
+         try {
+             const novaPessoaCriadas = await database.Pessoas.create(novaPessoa)
+             return res.status(200).json(novaPessoaCriadas)
+         } catch (error) {
+             return res.status(500).json(error.message)
+         }
+     }
+
+     static async atualizaPessoa(req, res) {
+         const { id } = req.params
+         const novasInfos = req.body
+         try {
+             await database.Pessoas.update(novasInfos, { where: { id: Number(id) } })
+             const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } })
+             return res.status(200).json(pessoaAtualizada)
+         } catch (error) {
+             return res.status(500).json(error.message)
+         }
+     }
+
+     static async apagarPessoa(req, res) {
+         const { id } = req.params
+         try {
+             await database.Pessoas.destroy({ where: { id: Number(id) } })
+             return res.status(200).json({ mensagem: `id ${id} deletado` })
+         } catch (error) {
+             return res.status(500).json(error.message)
+         }
+     }
  }
 
  module.exports = PessoaController
--- api/routes/pessoaRoute.js
+++ api/routes/pessoaRoute.js
@@ -4,5 +4,9 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegarTodasAsPessoa)
+router.get('/pessoas/:id', PessoaController.pegarUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.apagarPessoa)
 
 module.exports = router
